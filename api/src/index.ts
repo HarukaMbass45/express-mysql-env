c5ec46ecf2d7641b8ec809675d97dc9a4cf55946
KentaNakanishi <kenta_nakanishi@rebuild-inc.co.jp>
2023-02-18 14:52:14 +0900

    最小構成に変更

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,13 +2,6 @@ import express from 'express';
 import type { Request, Response, NextFunction } from 'express';
 const app: express.Express = express();
 
-type User = {
-  id: number;
-  name: string;
-  email: string;
-}
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS対応
@@ -23,16 +16,6 @@ app.listen(3000, () => {
   console.log('API Sever is running...');
 });
 
-const users: User[] = [
-  { id: 1, name: 'User1', email: 'user01@test.local', },
-  { id: 2, name: 'User2', email: 'user02@test.local', },
-  { id: 3, name: 'User3', email: 'user03@test.local', },
-];
-
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello Express!');
-});
-
-app.get('/users', (req: Request, res: Response,) => {
-  res.send(JSON.stringify(users));
 });
